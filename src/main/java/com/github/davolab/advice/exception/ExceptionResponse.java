f495f0efe3cb596c73d450e4485cfe0f9a0a80e8
shehara <shehara@davolab.com>
2023-11-13 13:53:40 +0530

    added latest

---
+++ src/main/java/com/github/davolab/advice/exception/ExceptionResponse.java
@@ -0,0 +1,18 @@
+package com.github.davolab.advice.exception;
+
+/**
+ * @author - Shehara
+ * @date - 1/25/2022
+ */
+
+public class ExceptionResponse {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/github/davolab/dao/support/SelectedFields.java
@@ -0,0 +1,13 @@
+package com.github.davolab.dao.support;
+
+public class SelectedFields {
+    private String fields;
+
+    public String getFields() {
+        return fields;
+    }
+
+    public void setFields(String fields) {
+        this.fields = fields;
+    }
+}
